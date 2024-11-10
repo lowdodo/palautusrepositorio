2fda1407ce9d69832f59024eb66328b877134868
lowdodo <lowdodo@ad.helsinki.fi>
2024-11-10 16:14:28 +0200

    moved to viikko2

--- git_branch_harjoitus/index.py
+++
@@ -1,9 +0,0 @@
-from logger import logger
-
-logger("aloitetaan")
-
-x = int(input("luku 1: "))
-y = int(input("luku 2: "))
-
-logger("lopetetaan")x = int(input("luku 1: "))
-y = int(input("luku 2: "))
