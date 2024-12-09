5e828f00d143d8511502cd9277bcbb775b32ebe9
lowdodo <dodoloksttrom@gmail.com>
2024-12-09 19:05:30 +0200

    tehtävä 2

---
+++ viikko6/query-language/src/index.py
@@ -0,0 +1,19 @@
+from statistics import Statistics
+from player_reader import PlayerReader
+from matchers import And, HasAtLeast, PlaysIn, Not, All, HasFewerThan
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
+    reader = PlayerReader(url)
+    stats = Statistics(reader)
+
+    matcher = And(
+        HasFewerThan(2, "goals"),
+        PlaysIn("NYR")
+    )
+
+    filtered_with_all = stats.matches(All())
+    print(len(filtered_with_all))
+
+if __name__ == "__main__":
+    main()
