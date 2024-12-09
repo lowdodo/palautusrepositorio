1753bb27183982945b910ba1e81de4910b629791
lowdodo <dodoloksttrom@gmail.com>
2024-12-09 22:50:43 +0200

    t2-5 lopullinen

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -6,17 +6,24 @@ def main():
     url = "https://studies.cs.helsinki.fi//nhlstats/2023-24/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
-
     query = QueryBuilder()
-
-    matcher = (
-      query
-      .plays_in("NYR")
-      .has_at_least(10, "goals")
-      .has_fewer_than(20, "goals")
-      .build()
+    m1 = (
+        query
+        .plays_in("PHI")
+        .has_at_least(10, "assists")
+        .has_fewer_than(10, "goals")
+        .build()
     )
+    m2 = (
+        QueryBuilder()
+        .plays_in("EDM")
+        .has_at_least(50, "points")
+        .build()
+    )
+    matcher = QueryBuilder().one_of(m1, m2).build()
+
     for player in stats.matches(matcher):
         print(player)
+
 if __name__ == "__main__":
     main()
